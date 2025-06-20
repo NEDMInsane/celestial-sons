8f7cea812f7d64acdc8ad3622d3f2ba953e08256
NEDMInsane <truxaln@gmail.com>
2025-06-20 12:01:53 -0400

    Maybe use TUI?

---
+++ src/celestialsons/Netcode.java
@@ -0,0 +1,11 @@
+package celestialsons;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Netcode {
+    ServerSocket server = new ServerSocket(27099);
+
+    public Netcode() throws IOException {
+    }
+}
